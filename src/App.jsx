09d85ad12263ddc6dc04b18fdbdb8e86c05e1d4c
crist-17 <pci1234good@gmail.com>
2025-08-13 09:23:41 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import {  BrowserRouter as Router, Route, Routes  } from "react-router-dom";
-
-
-// import { useState } from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./compo/Login";
 import Home from "./compo/Home";
@@ -11,26 +8,23 @@ import Sclass from "./class/Sclass";
 import Arr from "./comp22/Arr";
 
 function App() {
-
   return (
     <>
       <h1>-과제방-방방방</h1>
       <Router>
         <Routes>
-          <Route path="/react-site/" element={<Layout />}></Route>
-
-          <Route path="/react-site/" element={<Layout />} />
-          <Route index element={<Home />} />
-          <Route path="/react-site/login/" element={<Login />} />
-          <Route path="/react-site/login2/" element={<Login2 />} />
-          <Route path="/react-site/Sclass/" element={<Sclass />} />
-          <Route path="/react-site/Arr/" element={<Arr />} />
-
-
+          {/* 부모 라우트 하나 두고 자식으로 중첩 */}
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="login2" element={<Login2 />} />
+            <Route path="Sclass" element={<Sclass />} />
+            <Route path="Arr" element={<Arr />} />
+          </Route>
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
